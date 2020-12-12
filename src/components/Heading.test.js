136c541ecccea50feb1465305839ba3c5779c313
Fabiana-BP <fabi_bp91@hotmail.com>
2020-12-12 20:00:13 -0300

    tests

--- src/components/Heading.test.js
+++ src/components/Heading.test.js
@@ -5,7 +5,7 @@ import {render} from '@testing-library/react';
 import Heading from "./Heading";
 
 test('match snapshot',() => {
-  const {asFragment} = render(<Heading>Title</Heading>)
+  const {asFragment} = render(<Heading>Title</Heading>);
 
   expect(asFragment()).toMatchSnapshot();
 });
