ae728c51d0812860e3b3790a952e6d5ca9dd1571
Fabiana-BP <fabi_bp91@hotmail.com>
2020-12-16 20:52:32 -0300

    Add a button in Hero docs

--- src/components/Hero.stories.js
+++ src/components/Hero.stories.js
@@ -5,26 +5,28 @@ import Hero from "./Hero";
 import BgHighWayImage from "../stories/assets/bg-highway.jpg";
 import BgCarImage from "../stories/assets/bg-car.webp";
 import Heading from "./Heading";
+import Button from "./Button";
 
 export default { title: "Components/Hero", component: Hero };
 
 export const usage = () => (
   <Hero image={BgCarImage}>
     <Heading>
-      <h1>{text("Title","Ganhe sua liberdade para ir e vir")}  
+      <h1>
+        Ganhe sua <strong>liberdade</strong>
+        <br />
+        para ir e vir
       </h1>
     </Heading>
     <p>{text("Text","A auto escola lider em aprovação")}</p>
+    <Button variant='outlined' color='primary'>Matricule-se agora</Button>
   </Hero>
 );
 
 export const withList = () => (
   <Hero image={BgHighWayImage}>
     <Heading>
-      <h1>
-        Ganhe sua <strong>liberdade</strong>
-        <br />
-        para ir e vir
+      <h1>{text("Title","Ganhe sua liberdade para ir e vir")}  
       </h1>
     </Heading>
     <ul>
@@ -42,5 +44,6 @@ export const withList = () => (
         Internet.
       </li>
     </ul>
+    <Button variant='outlined' color='primary'>Matricule-se agora</Button>
   </Hero>
 );
