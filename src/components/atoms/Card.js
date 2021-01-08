b34de5870000ad42b8b9981dd58cc90b112ee9c5
Fabiana-BP <fabi_bp91@hotmail.com>
2021-01-08 01:05:55 -0300

    Fix Card Component

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -50,6 +50,7 @@ CardMedia.defaultProps = {
 const StyledCard = styled.div`
   background-color: #fff;
   border-radius: 4px;
+  overflow: hidden;
 `;
 
 const Card = ({ children }) => <StyledCard>{children}</StyledCard>;
