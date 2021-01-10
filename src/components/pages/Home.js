309a640df4ffa5933c03c8218bbe95aa75ea33a5
Fabiana-BP <fabi_bp91@hotmail.com>
2021-01-09 21:02:55 -0300

    corrections

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon } from "react-icons/fa";
 
 import Hero from "components/molecules/Hero";
@@ -166,8 +165,5 @@ const Home = () => (
     <Footer />
   </>
 );
-
-Home.defaultProps = {};
-Home.propTypes = {};
 
 export default Home;
