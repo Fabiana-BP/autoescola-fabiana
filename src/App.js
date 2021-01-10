a9251bf29429908ce02affe13bbfa0b9aa6b245e
Fabiana-BP <fabi_bp91@hotmail.com>
2021-01-09 21:02:36 -0300

    corrections.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon} from 'react-icons/fa';
-
 import Home from "components/pages/Home";
 import ThemeProvider from "styles/ThemeProvider";
 import GlobalStyle from "styles/GlobalStyle";
-
 
 function App() {
   return (
     <ThemeProvider>
       <GlobalStyle />
-        <Home/>
+      <Home />
     </ThemeProvider>
   );
 }
